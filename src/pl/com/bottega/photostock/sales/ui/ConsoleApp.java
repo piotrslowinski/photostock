dc85985562e18506159937b61442fc0037dce57b
piotrslowinski <piotrslowinski89@gmail.com>
2017-08-20 10:02:39 +0200

    first

---
+++ src/pl/com/bottega/photostock/sales/ui/ConsoleApp.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.photostock.sales.ui;
+
+/**
+ * Created by user on 06.08.2017.
+ */
+public class ConsoleApp {
+}
---
+++ test/pl/com/bottega/photostock/sales/model/MoneyTest.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.photostock.sales.model;
+
+/**
+ * Created by user on 19.08.2017.
+ */
+public class MoneyTest {
+}
